0ba06be134c73f45122c3b66d94e5c10d12661ea
yuyan <3023624599@qq.com>
2023-05-27 19:11:50 +0800

    commit controller

---
+++ src/main/java/com/huawei/interstellar/service/UserSerive.java
@@ -0,0 +1,11 @@
+package com.huawei.interstellar.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: sugar
+ * @Date: 2023/04/09/23:15
+ * @Description:
+ */
+public interface UserSerive {
+}
---
+++ src/main/java/com/huawei/interstellar/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package com.huawei.interstellar.service.impl;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: sugar
+ * @Date: 2023/04/09/23:16
+ * @Description:
+ */
+@Component
+public class UserServiceImpl {
+    public void test(){
+        System.out.println("hello");
+    }
+}
