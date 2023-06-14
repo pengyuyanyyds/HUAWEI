09857ca351aac24529930e4c62b6298f469ba3e0
yuyan <3023624599@qq.com>
2023-06-14 22:38:13 +0800

    现在在新分支need2上面修改代码并进行提交

--- src/test/java/com/huawei/interstellar/Test01.java
+++ src/test/java/com/huawei/interstellar/Test01.java
@@ -11,5 +11,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("this is the branch master");
         System.out.println("now, I change code in master and checkout to new branch");
+        System.out.println("now, I have the new branch and i change code in this branch");
     }
 }
