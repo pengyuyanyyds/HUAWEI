b39eaec89be8596088e7f493616372ffe2197fd2
yuyan <3023624599@qq.com>
2023-06-14 22:23:26 +0800

    我现在在master分支提交代码

---
+++ src/test/java/com/huawei/interstellar/Test01.java
@@ -0,0 +1,14 @@
+package com.huawei.interstellar;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: sugar
+ * @Date: 2023/06/14/22:21
+ * @Description:
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("this is the branch master");
+    }
+}
